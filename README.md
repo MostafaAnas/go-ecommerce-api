346190b1396fb45d260e28c1614384049a6b1ad2
MostafaAnas <mostafahelal120@gmail.com>
2024-10-30 17:27:24 +0300

    add database connection

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # go-ecommerce-api
+
+run mysql 
+
+```
+docker run --name mysql -e MYSQL_ROOT_PASSWORD=mypassword -d -p 3306:3306 -v mysql-data:/var/lib/mysql mysql:9.1.0
+mysql -u root -p -h 127.0.0.1 -P 3306
+CREATE DATABASE ecommerce;
+```
--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/MostafaAnas/go-ecommerce-api.git/cmd/api"
+	"github.com/MostafaAnas/go-ecommerce-api.git/config"
+	"github.com/MostafaAnas/go-ecommerce-api.git/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User: config.Envs.DBUser,
+		Passwd: config.Envs.DBPassword,
+		Addr: config.Envs.DBAddr,
+		DBName: config.Envs.DBName,
+		Net: "tcp",
+		AllowNativePasswords: true,
+		ParseTime: true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+	
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB){
+	err := db.Ping()
+	if err != nil{
+		log.Fatal(err)
+	}
+	log.Println("DB Successfully connected")
 }
